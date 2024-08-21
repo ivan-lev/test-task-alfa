fbcb85b2bb3d509ca3a428a357695342114ac619
Ivan Lev <fanatos@mail.ru>
2024-08-21 11:43:14 +0500

    feat: add book type

---
+++ src/types/book.ts
@@ -0,0 +1,11 @@
+export interface Book {
+  key: string;
+  author_name: string[];
+  cover_edition_key: string;
+  first_sentence: string[];
+  format: string[];
+  isbn: string[];
+  first_publish_year: number;
+  title: string;
+  cover?: string;
+}
