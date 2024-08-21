016f6b482c2ca77f40af00a26b241017a3346c51
Ivan Lev <fanatos@mail.ru>
2024-08-21 12:15:26 +0500

    fead: add Card layout and like functionality

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,28 @@
 import './Card.scss';
 
-export default function Card(): JSX.Element {
-  return <div></div>;
+import type { Book } from '../../types/book';
+
+import { likeBook } from '../../slices/booksSlice';
+import { useDispatch } from 'react-redux';
+
+export default function Card({ book }: { book: Book }): JSX.Element {
+  const dispatch = useDispatch();
+
+  const { key, cover, title, isLiked } = book;
+  const bookLink = key.split('/').pop();
+
+  return (
+    <div className="card">
+      <a href={bookLink}>
+        <img className="card__image" src={cover} alt={`Обложка к ${title}`}></img>
+      </a>
+      <a href={bookLink}>{title}</a>
+      <img
+        src={`./icons/heart-${isLiked ? 'filled' : 'empty'}.svg`}
+        alt=""
+        className="card__like"
+        onClick={() => dispatch(likeBook(key))}
+      />
+    </div>
+  );
 }
