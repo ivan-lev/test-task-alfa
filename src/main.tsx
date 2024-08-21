b40b28aaabf8878edf963fa3ab149b0e71648409
Ivan Lev <fanatos@mail.ru>
2024-08-21 12:16:00 +0500

    feat: cover App in redux store provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App/App.tsx';
 import './index.scss';
 
+import { Provider } from 'react-redux';
+import store from './slices/index.ts';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
