cdc55b27de1233fb8201463ccf49b48abb0c91da
Ivan Lev <fanatos@mail.ru>
2024-08-21 23:12:39 +0500

    fix: try to make universal settings fro deployed and local environment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/test-task-alfa/'
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/test-task-alfa/'
+// });
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/'
+  };
+
+  if (command !== 'serve') {
+    config.base = '/test-task-alfa/';
+  }
+
+  return config;
 });
