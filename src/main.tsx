0ec969f9956cf94a83592d23851f8781b1c95635
Ivan Lev <fanatos@mail.ru>
2024-08-21 15:19:10 +0500

    feat: cover App with react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
+import './index.scss';
+
+// React
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// React Router
+import { BrowserRouter } from 'react-router-dom';
+
+// Components
 import App from './components/App/App.tsx';
-import './index.scss';
 
 import { Provider } from 'react-redux';
 import store from './slices/index.ts';
@@ -9,7 +16,9 @@ import store from './slices/index.ts';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
