59861999739cb3304b08d8aa5b5acafd0ad7c527
Ivan Lev <fanatos@mail.ru>
2024-08-22 10:26:40 +0500

    fix: another try to deploy on pages and run app localy

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 // export default defineConfig({
-//   plugins: [react()],
-//   base: '/test-task-alfa/'
+//   plugins: [react()]
 // });
 
 export default defineConfig(({ command }) => {
