077ef677917ded881edfd9fa726605bcbecc3b48
Ivan Lev <fanatos@mail.ru>
2024-08-22 12:04:23 +0500

    Feat: implement Logo in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,12 @@
 import './Header.scss';
 
+import Logo from '../Logo/Logo';
+
 export default function Header() {
-  return <header className="header">Тестовое задание для Alfa</header>;
+  return (
+    <header className="header">
+      <Logo />
+      <span className="header__title">Тестово задание для ООО Экосистема Альфа</span>
+    </header>
+  );
 }
